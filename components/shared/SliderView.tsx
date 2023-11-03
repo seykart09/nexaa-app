48d3574a09108e995b241226e51113898604cc8e
Seyram Kartey <seyram.kartey@gmail.com>
2023-11-03 00:47:00 +0000

    fixing react slick build error by removing slick out

--- components/shared/SliderView.tsx
+++ components/shared/SliderView.tsx
@@ -1,8 +1,5 @@
 import React from 'react'
-// import Slider from 'react-slick';
-// import "slick-carousel/slick/slick.css"; 
-// import "slick-carousel/slick/slick-theme.css";
-// import Image from 'next/image';
+
 
 
 const SliderView = ({}) => {
