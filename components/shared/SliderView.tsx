7843fa783cf27210d5b1de941af0fd2705be8d21
Seyram Kartey <seyram.kartey@gmail.com>
2023-11-03 00:41:07 +0000

    fixing react slick build error by commenting slick out

--- components/shared/SliderView.tsx
+++ components/shared/SliderView.tsx
@@ -1,28 +1,27 @@
 import React from 'react'
-import Slider from 'react-slick';
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
-import Image from 'next/image';
+// import Slider from 'react-slick';
+// import "slick-carousel/slick/slick.css"; 
+// import "slick-carousel/slick/slick-theme.css";
+// import Image from 'next/image';
 
 
-const SliderView = ({data}) => {
-    const settings = {
-        dots: true,
-        infinite: true,
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        centerPadding: "60px",
-        autoplay: true,
-        speed: 2000,
-        autoplaySpeed: 2000,
-        cssEase: "linear"
-    };
+const SliderView = ({}) => {
+    // const settings = {
+    //     dots: true,
+    //     infinite: true,
+    //     slidesToShow: 5,
+    //     slidesToScroll: 1,
+    //     centerPadding: "60px",
+    //     autoplay: true,
+    //     speed: 2000,
+    //     autoplaySpeed: 2000,
+    //     cssEase: "linear"
+    // };
 
-    console.log(data);
 
     return (
         <div className='space-x-4'>
-            <Slider {...settings}>
+            {/* <Slider {...settings}>
                 {data.map((item:any,index:number) =>(
                     <div key={index} className="relative mr-4" style={{width:300, height:400}}>
                         <Image
@@ -32,7 +31,8 @@ const SliderView = ({data}) => {
                         />
                     </div>
                 ))}
-            </Slider>
+            </Slider> */}
+            test
         </div>
     )
 }
